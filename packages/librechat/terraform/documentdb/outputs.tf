9a9b544d56225aa1a1fb0d41bffc3147bed32ca3
Collin Bolles <collinbolles@gmail.com>
2025-02-12 12:03:18 -0500

    Leverage documentDB endpoint from terraform

--- packages/librechat/terraform/documentdb/outputs.tf
+++ packages/librechat/terraform/documentdb/outputs.tf
@@ -5,6 +5,6 @@
 #
 
 output "database_librechat" {
-  value = "mongodb://aida:${jsondecode(aws_secretsmanager_secret_version.documentdb_password_secret_value.secret_string).password}@aida-documentdb-20250207200016537900000001.cyimouge4gmi.us-east-1.docdb.amazonaws.com:27017/?retryWrites=false"
+  value = "mongodb://aida:${jsondecode(aws_secretsmanager_secret_version.documentdb_password_secret_value.secret_string).password}@${aws_docdb_cluster.aida_documentdb.endpoint}:27017/?retryWrites=false"
   sensitive = true
 }
