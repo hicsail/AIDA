5416cef35abaf1c5a3250b7aecf3c5e69a44845b
Collin Bolles <collinbolles@gmail.com>
2025-02-12 14:05:26 -0500

    Extract ingress rule

--- packages/librechat/terraform/librechat/main.tf
+++ packages/librechat/terraform/librechat/main.tf
@@ -210,21 +210,15 @@ resource "aws_security_group" "librechat_sg" {
   name        = "librechat-sg"
   description = "Allow inbound traffic to Fargate service"
   vpc_id      = var.vpc_id
+}
 
-  ingress {
-    from_port   = 3080
-    to_port     = 3080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "librechat_ingress" {
+  security_group_id = aws_security_group.librechat_sg.id
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  cidr_ipv4   = "10.0.0.0/16"
+  from_port   = 3080
+  ip_protocol = "tcp"
+  to_port     = 3080
 }
 
 # Fargate Service
