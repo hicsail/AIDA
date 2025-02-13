37f9d4192de2b474f32cdfa528215673cbc8565c
Collin Bolles <collinbolles@gmail.com>
2025-02-13 12:17:09 -0500

    Begin work on making bedrock user

---
+++ packages/librechat/terraform/bedrock/main.tf
@@ -0,0 +1,30 @@
+# IAM user for bedrock access
+resource "aws_iam_user" "aida_bedrock" {
+  name = "aida-bedrock"
+}
+
+# Create the bedrock access policy
+resource "aws_iam_policy" "aida_bedrock_policy" {
+  name        = "BedrockQueryPolicy"
+  description = "Policy to allow querying AWS Bedrock models"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "bedrock:InvokeModel",
+          "bedrock:InvokeModelWithResponseStream"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the user
+resource "aws_iam_user_policy_attachment" "bedrock_attachment" {
+  user       = aws_iam_user.aida_bedrock.name
+  policy_arn = aws_iam_policy.aida_bedrock_policy.arn
+}
