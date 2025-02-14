f4e6cf6fb4d9e2cc328d1e6ea20414650ff559c3
Collin Bolles <collinbolles@gmail.com>
2025-02-14 11:50:30 -0500

    Update public routing

--- packages/librechat/terraform/vpc/main.tf
+++ packages/librechat/terraform/vpc/main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "aida_private_subnet" {
 }
 
 resource "aws_internet_gateway" "main_gw" {
-  vpc_id    = aws_vpc.aida_vpc.id
+  vpc_id = aws_vpc.aida_vpc.id
 }
 
 resource "aws_route_table" "public_rt" {
@@ -38,26 +38,30 @@ resource "aws_route_table_association" "public_rt_assoc" {
 }
 
 resource "aws_eip" "nat_eip" {
+  count  = 2
   domain = "vpc"
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.aida_public_subnet[0].id
+  count         = 2
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id     = aws_subnet.aida_public_subnet[count.index].id
 }
 
 resource "aws_route_table" "private_rt" {
+  count  = 2
   vpc_id = aws_vpc.aida_vpc.id
 }
 
 resource "aws_route" "private_nat_route" {
-  route_table_id         = aws_route_table.private_rt.id
+  count                  = 2
+  route_table_id         = aws_route_table.private_rt[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway[count.index].id
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
   count          = length(aws_subnet.aida_private_subnet)
   subnet_id      = aws_subnet.aida_private_subnet[count.index].id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.private_rt[count.index].id
 }
